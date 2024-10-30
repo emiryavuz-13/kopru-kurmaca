8d1853c62cd672e972b8b94c6ccdd1b8eef761af
Emir-yavuz <emirsemihzehra@gmail.com>
2024-10-31 01:50:05 +0300

    harita tasarım kodları eklendi

---
+++ map_genarator.py
@@ -0,0 +1,25 @@
+def bos_deniz_fonk():
+    global harita_tip
+    harita_tip="boş"
+    resim_bastirici()
+def seyrek_tasli_deniz_fonk():
+    global harita_tip
+    harita_tip="seyrek"
+    resim_bastirici()
+def tasli_deniz_fonk():
+    global harita_tip
+    harita_tip="taşlı"
+    resim_bastirici()
+
+def kucuk_harita_fonk():
+    global harita_buyukluk
+    harita_buyukluk=5
+    resim_bastirici()
+def orta_harita_fonk():
+    global harita_buyukluk
+    harita_buyukluk=7
+    resim_bastirici()
+def buyuk_harita_fonk():
+    global harita_buyukluk
+    harita_buyukluk=9
+    resim_bastirici()
