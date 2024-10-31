7e707e9dba7d601b3bbb289acbbbdef36e2dec03
Emir-yavuz <emirsemihzehra@gmail.com>
2024-11-01 02:02:34 +0300

    Oyun kuralları yazıldı(yol var mı, skor hesaplama)

--- utils.py
+++ utils.py
@@ -52,6 +52,94 @@ def izin_var_mi(i,j,oyuncu): #1. oyuncu için butona basma hakkı var mı sorgus
 
     return False
 
+def yol_var_mi_fonk(x,y):  #Yol var mı denetimini yapan fonks
+
+    global basladin_mi,yol_var_mi
+    if basladin_mi:
+        for i in range(len(matrix)-1):
+            if matrix[i][y]>0:
+
+                basladin_mi=False
+
+                try:
+                    yol_var_mi_fonk(i-1,y+1)
+                except:
+                    print("!!")
+
+                yol_var_mi_fonk(i,y+1)
+
+                try:
+                    yol_var_mi_fonk(i+1,y+1)
+                except:
+                    print("!!")
+
+
+    if matrix[x][y]>0 and y!=len(matrix)-1 and x!=len(matrix)-1:
+
+        try:
+            if x!=0:
+                yol_var_mi_fonk(x-1,y+1)
+        except:
+            print("!!")
+        try:
+            yol_var_mi_fonk(x,y+1)
+        except:
+            print("!!")
+
+        yol_var_mi_fonk(x+1,y+1)
+
+    if y==len(matrix)-1 and matrix[x][y]>0:
+
+        yol_var_mi=True
+
+
+def skor_hesaplama_fonk():
+    boyut=len(skor_matrix)
+    global matrix_okuma_bool,skor_hesaplama_bool
+    if skor_hesaplama_bool: #İlk girişince taşlara -1 değerini ver bi daha buraya girme
+        for i in range(boyut):
+            for j in range(boyut):
+                if matrix[i][j] == -1:
+                    skor_matrix[i][j] = -1
+        skor_hesaplama_bool=False
+
+    matrix_okuma_bool=False
+    uzunluk=boyut-1
+    orta=int(boyut/2)+1
+    for i in range(boyut):
+        if i==0 and skor_matrix[i][uzunluk-1]<0 and skor_matrix[i+1][uzunluk-1]<0:  #prnt.sc/241emm2
+            if skor_matrix[i][uzunluk]>=0:
+                matrix_okuma_bool=True
+                skor_matrix[i][uzunluk]=-2
+
+        if i==uzunluk and skor_matrix[i][uzunluk-1]<0 and skor_matrix[i-1][uzunluk-1]<0:  #prnt.sc/241f3ad
+            if skor_matrix[i][uzunluk] >= 0:
+                matrix_okuma_bool = True
+                skor_matrix[i][uzunluk]=-2
+
+        if i>=orta-1 and i!=uzunluk: #ortadan sütundan sondan bir önceki sütuna kadar tara  prnt.sc/24502ap
+            stack=0
+            for j in range(boyut):
+                if skor_matrix[j][i]<0:
+                   stack+=1
+                else:
+                    stack=0
+                if j==1 and stack==2:
+                    if skor_matrix[0][i+1] >= 0:
+                        matrix_okuma_bool = True
+                        skor_matrix[0][i+1]=-2
+                if stack>=3:
+                    if skor_matrix[j-1][i+1] >= 0:
+                        matrix_okuma_bool = True
+                        skor_matrix[j-1][i+1]=-2
+                if j==boyut-1 and stack>=2:
+                    if skor_matrix[boyut-1][i+1] >= 0:
+                        matrix_okuma_bool = True
+                        skor_matrix[boyut-1][i+1]=-2
+    if matrix_okuma_bool:
+        skor_hesaplama_fonk()
+
+
 def matrix_okuma(dosyam):
     icerik = dosyam.read()
     skor_matrixx = []
